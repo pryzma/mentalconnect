62d3d554ee7cf81dac56f4d720cb709c0fd2d0a4
Fides <fides.torres@outlook.com>
2020-11-12 20:40:17 +0100

    Hover text links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="css/app.css" />
         <link rel="icon" type="image/png" href="img/favicon.png" />
-        <title> {{ config('app.name') }}</title>
-
+        <title>{{ config('app.name') }}</title>
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -16,21 +15,17 @@
                         <a href="{{ url('/home') }}"> <i class="fas fa-home"></i> Home</a>
                     @else
                         <a href="{{ route('login') }}"> <i class="fas fa-sign-in-alt"></i> Login</a>
-
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}"> <i class="fas fa-user-plus"></i> Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    <span class="inner"> {{ config('app.name') }}</span>
+                    <span class="inner">{{ config('app.name') }}</span>
                 </div>
-
                 <div class="links">
-              
                     <a href="https://trello.com/b/KqZd6nte/mentalconnect" target="_blank">Trello</a>
                     <a href="https://github.com/pryzma/mentalconnect" target="_blank">GitHub</a>
                 </div>
